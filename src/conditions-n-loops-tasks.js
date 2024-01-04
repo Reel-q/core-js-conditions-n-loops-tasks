679205348a9e2d21e298a72bdcee76ebb9dfc52c
Reel <ladias231299@gmail.com>
2024-01-04 20:52:06 +0200

    feat: add solution for 13 task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -401,8 +401,20 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const answer = arr;
+
+  for (let i = 0; i < answer.length - 1; i += 1) {
+    for (let j = 0; j < answer.length - 1 - i; j += 1) {
+      if (answer[j] > answer[j + 1]) {
+        const temporary = answer[j];
+        answer[j] = answer[j + 1];
+        answer[j + 1] = temporary;
+      }
+    }
+  }
+
+  return answer;
 }
 
 /**
